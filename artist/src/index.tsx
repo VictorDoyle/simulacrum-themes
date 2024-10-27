a76f0ed5fe805e93c92e56aac67365ffaf6505f2
VictorDoyle <victor-doyle@outlook.com>
2024-10-27 13:48:43 -0400

    base setup

---
+++ artist/src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <div>Welcome to the Artist Template!</div>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
